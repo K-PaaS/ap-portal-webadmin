12ee26e6ac52f1f8b322b80f7cd152164be66f1a
okpc579 <ski579579@naver.com>
2023-09-13 15:50:42 +0900

    kpaas word change

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# PAAS-TA-PORTAL-WEBADMIN
+# ap-portal-webadmin
 
 ## PORTAL WEBADMIN
 PORTAL WEBADMIN는 CCDB, UAADB, PORTALDB로부터 데이터를 조회하고 클라우드 컨트롤러에서 제공(Portal Api, Portal Common Api 어플리케이션)하는 REST API 호출을 통해 상호작용하며, 웹 사용자 인터페이스 환경에서 플랫폼을 관리할 수 있도록 한다.
---
+++ src/main/java/org/openpaas/portal/web/admin/config/ApplicationConfig.java
@@ -0,0 +1,126 @@
+package org.openpaas.portal.web.admin.config;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.tomcat.jni.Local;
+import org.openpaas.portal.web.admin.config.security.CustomIntercepter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.context.request.async.TimeoutCallableProcessingInterceptor;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * The type Application config.
+ */
+@EnableWebMvc
+@Configuration
+//@ComponentScan({ "com.openpaas.portal.web.admin.*" })
+public class ApplicationConfig extends WebMvcConfigurerAdapter {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(ApplicationConfig.class);
+
+    @Autowired
+    LanguageConfig languageConfig;
+
+    /**
+     * View resolver internal resource view resolver.
+     *
+     * @return the internal resource view resolver
+     */
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver resolver = new CookieLocaleResolver();
+        String defaultLang = languageConfig.getLanguageList().get(0);
+        resolver.setDefaultLocale(new Locale(defaultLang));
+        resolver.setCookieName("lang");
+        return resolver;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("/messages/message");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(mappingJackson2HttpMessageConverter());
+    }
+
+    /**
+     * Mapping jackson 2 http message converter mapping jackson 2 http message converter.
+     *
+     * @return the mapping jackson 2 http message converter
+     */
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false));
+        return converter;
+    }
+
+    @Bean
+    CustomIntercepter customIntercepter() {
+        return new CustomIntercepter();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(customIntercepter());
+    }
+
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer() {
+        return (container -> {
+            container.setSessionTimeout(1000);  // session timeout value
+        });
+    }
+
+
+
+    @Override
+    public void configureAsyncSupport(final AsyncSupportConfigurer configurer) {
+        configurer.setDefaultTimeout(10 * 1000L);
+        configurer.registerCallableInterceptors(timeoutInterceptor());
+    }
+
+    @Bean
+    public TimeoutCallableProcessingInterceptor timeoutInterceptor() {
+        return new TimeoutCallableProcessingInterceptor();
+    }
+
+}
